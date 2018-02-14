6ba623a0f4dd9c95d0a4e4e6e2eaed39b598562f
Joshua Firkin <joshuafirkin@hotmail.co.uk>
2018-02-14 15:54:55 +0000

    Initial Commit
    Added very basic FPS controls to the game.
    Currently working on adding weapons.

---
+++ GoldeneyeProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    private PlayerMotor motor;
+
+    public float movementSpeed = 10.0f;
+    public float rotationSensitivity = 10.0f;
+
+    void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    void Update()
+    {
+        // Applying movement to the character.
+        Vector3 horMovement = transform.right * Input.GetAxis("Horizontal");
+        Vector3 verMovement = transform.forward * Input.GetAxis("Vertical");
+
+        Vector3 vel = (horMovement + verMovement).normalized * movementSpeed;
+        motor.AddMovement(vel);
+
+
+        // Applying rotation to the character.
+        float yRotation = Input.GetAxis("LookVertical");
+        Vector3 rotation = new Vector3(0.0f, yRotation, 0.0f) * rotationSensitivity;
+        motor.AddRotation(rotation);
+
+        //Applying rotation to the camera.
+        float xRotation = Input.GetAxis("LookHorizontal");
+        Vector3 camRotation = new Vector3(xRotation, 0.0f, 0.0f) * rotationSensitivity;
+        motor.AddCameraRotation(camRotation);
+    }
+}
---
+++ GoldeneyeProject/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    [SerializeField]
+    private Camera cam;
+
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+    private Vector3 camRotation = Vector3.zero;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void AddMovement(Vector3 vel)
+    {
+        velocity = vel;
+    }
+
+    public void AddRotation(Vector3 rot)
+    {
+        rotation = rot;
+    }
+
+    public void AddCameraRotation(Vector3 rot)
+    {
+        camRotation = rot;
+    }
+
+    void FixedUpdate()
+    {
+        if (velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + (velocity * Time.fixedDeltaTime));
+        }
+
+        if (rotation != Vector3.zero)
+        {
+            rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation * Time.fixedDeltaTime));
+        }
+
+        if (camRotation != Vector3.zero)
+        {
+            if (cam != null)
+            {
+                cam.transform.Rotate(-camRotation * Time.deltaTime);
+            }
+        }
+    }
+}
